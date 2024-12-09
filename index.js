ecaf98d2f161d1c747579b4f0077aedadb465096
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-12-09 12:40:44 +0530

    add

--- index.js
+++ index.js
@@ -14,17 +14,27 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/files", express.static("files"));
 
-if (process.env.mode === 'production') {
-    app.use(cors())
-} else {
-    app.use(cors({
-        origin: ["http://localhost:5174", "http://localhost:5173"],
-        credentials: true,
-    }))
-}
+
+app.use(cors({
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'http://neurocort-frontend.s3-website.ap-south-1.amazonaws.com',
+            'http://localhost:5173', 
+        ];
+
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
+
 
 app.use(session({
-    secret: "erjgqrg5ergwgwt2hw5twjgh534BjugKJ", //Random key
+    secret: "erjgqrg5ergwgwt2hw5twjgh534BjugKJ", //random key
     resave: false,
     saveUninitialized: true,
 }))
@@ -36,5 +46,6 @@ app.use('/', require('./routes/userRoutes'))
 app.use('/', require('./routes/blogRoutes'))
 
 db_connect()
+
 
 app.listen(port, () => console.log(`Server running on port ${port}!`))
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const userController = require('../controller/userController')
 
 // Subscription route
-router.post('/api/susbscribe', userController.userSubscribe)
+router.post('/api/subscribe', userController.userSubscribe)
 
 module.exports = router 
